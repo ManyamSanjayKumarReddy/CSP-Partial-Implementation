6e761d4a1c3f57167c97f4fa15d7b4c21d93a6ee
Manyam Sanjay Kumar Reddy <94047241+ManyamSanjayKumarReddy@users.noreply.github.com>
2023-02-06 13:07:22 +0530

    Add files via upload

---
+++ base/urls.py
@@ -0,0 +1,20 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('login/', views.Loginpage, name='login'),
+    path('logout/', views.LogoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
+
+
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.createRoom,name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom , name='update-room'),
+    path('delete-room/<str:pk>/', views.deleteRoom , name='delete-room'),
+]
+
+
